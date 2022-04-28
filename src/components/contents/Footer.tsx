6f5989973113948b402d7bfb65aa359255f3f93a
Hamin Park <haminjp@naver.com>
2022-04-28 18:29:25 +0900

    [추가] Footer 컴포넌트 추가

---
+++ src/components/contents/Footer.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { FocusPosition } from "../../pages/HDPage";
+
+const FOOTER_ROW = 2;
+
+interface FooterProps {
+  valueArray: string[][];
+  currentFocus: FocusPosition;
+}
+
+interface FooterItemProps {
+  value: string;
+  index: number;
+  currentFocus: FocusPosition;
+}
+
+const FooterContainer = styled.div<{ isFocused: boolean }>`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  background-color: lightblue;
+  border: ${(props) =>
+    props.isFocused ? "5px solid blue" : "5px dotted black"};
+`;
+
+const FooterItemContainer = styled.div<{ isFocused: boolean }>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: ${(props) =>
+    props.isFocused ? "5px solid red" : "5px dotted black"};
+`;
+
+const FooterItem = ({ value, index, currentFocus }: FooterItemProps) => {
+  const isFocused =
+    FOOTER_ROW === currentFocus.row && index === currentFocus.column;
+  return (
+    <FooterItemContainer isFocused={isFocused}>{value}</FooterItemContainer>
+  );
+};
+
+const Footer = ({ valueArray, currentFocus }: FooterProps) => {
+  const FooterValues = valueArray[FOOTER_ROW];
+
+  return (
+    <FooterContainer isFocused={FOOTER_ROW === currentFocus.row}>
+      {FooterValues.map((value, index) => (
+        <FooterItem
+          key={index}
+          index={index}
+          value={value}
+          currentFocus={currentFocus}
+        />
+      ))}
+    </FooterContainer>
+  );
+};
+
+export default Footer;
--- src/components/contents/Contents.tsx
+++ src/components/contents/Contents.tsx
@@ -1,57 +1,6 @@
-// const BodyContainer = styled.div`
-//   width: 100%;
-//   flex: 1;
-//   display: flex;
-//   background-color: lightgreen;
-// `;
-
-// const BodyElement = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   border: 1px solid black;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const FooterContainer = styled.div`
-//   width: 100%;
-//   height: 70px;
-//   display: flex;
-//   background-color: lightblue;
-// `;
-
-// const FooterElement = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   border: 1px solid black;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-
-// const Body = () => {
-//   return (
-//     <BodyContainer>
-//       <BodyElement>Content 1</BodyElement>
-//       <BodyElement>Content 2</BodyElement>
-//       <BodyElement>Content 3</BodyElement>
-//       <BodyElement>Content 4</BodyElement>
-//       <BodyElement>Content 5</BodyElement>
-//     </BodyContainer>
-//   );
-// };
-
-// const Footer = () => {
-//   return (
-//     <FooterContainer>
-//       <FooterElement>Footer 1</FooterElement>
-//       <FooterElement>Footer 2</FooterElement>
-//     </FooterContainer>
-//   );
-// };
-
 import styled from "styled-components";
 import { FocusPosition } from "../../pages/HDPage";
+import Footer from "./\bFooter";
 import Body from "./Body";
 import Header from "./Header";
 
@@ -72,7 +21,7 @@ const Contents = ({ valueArray, currentFocus }: ContentsProps) => {
     <ContentsContainer>
       <Header valueArray={valueArray} currentFocus={currentFocus} />
       <Body valueArray={valueArray} currentFocus={currentFocus} />
-      {/* <Footer /> */}
+      <Footer valueArray={valueArray} currentFocus={currentFocus} />
     </ContentsContainer>
   );
 };
